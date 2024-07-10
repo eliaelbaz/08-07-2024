6df239229cf91183e800cb15dad92ca6f40ac99e
eliaelbaz <eliaelbaz050@gmail.com>
2024-07-10 10:17:40 +0300

    Update hw_3_1.py

--- hw_3_1.py
+++ hw_3_1.py
@@ -4,4 +4,5 @@ first_name: str = str (input("Enter your first name: "))
 last_name: str = str (input("Enter your last name: "))
 height: float = float(input("Enter your height: "))
 year_of_birth: int = int(input("Enter your year of birth: "))
-print (f" id: {id_user} name: {last_name}, {first_name} height: {height} year-of-birth: {year_of_birth}")
+print (f"id: {id_user} name: {last_name}_{first_name} height: {height} year-of-birth: {year_of_birth}")
+##End
