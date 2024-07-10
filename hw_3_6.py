98703a927c9cb710cbb1c902b72d8a18ca734bb4
eliaelbaz <eliaelbaz050@gmail.com>
2024-07-10 09:48:16 +0300

    Update hw_3_6.py

--- hw_3_6.py
+++ hw_3_6.py
@@ -14,7 +14,7 @@ first_name3: str = str(input("Enter your first name: "));
 last_name3: str = str(input("Enter your last name: "));
 height3: float = float(input("Enter your height: "));
 year_of_birth3: int = int(input("Enter your year of birth: "));
-user1_details = f"#: {id_user1:<5} name: {last_name1[:10]:<10} {first_name1[:10]:<10} height: {height1:.2f} year-of-birth: {year_of_birth1:<4}"
+user1_details = f"#id: {id_user1:<5} name: {last_name1[:10]:<10} {first_name1[:10]:<10} height: {height1:.2f} year-of-birth: {year_of_birth1:<4}"
 user2_details = f"#id: {id_user2:<5} name: {last_name2[:10]:<10} {first_name2[:10]:<10} height: {height2:.2f} year-of-birth: {year_of_birth2:<4}"
 user3_details = f"#id: {id_user3:<5} name: {last_name3[:10]:<10} {first_name3[:10]:<10} height: {height3:.2f} year-of-birth: {year_of_birth3:<4}"
 print ("\nDetails of users in a formatted table:");
